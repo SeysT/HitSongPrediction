55d4f3af97af6a74dfd04847a620df11bc3f926d
SeysT <thibaut.seys@hotmail.fr>
2018-04-18 17:54:05 +0200

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+mkdir -p Data
 cd Data/
 
 wget http://hog.ee.columbia.edu/craffel/lmd/lmd_matched.tar.gz
 wget http://hog.ee.columbia.edu/craffel/lmd/lmd_matched_h5.tar.gz
 wget http://hog.ee.columbia.edu/craffel/lmd/match_scores.json
 
-tar -xvf lmd_matched
-tar -xvf lmd_matched_h5
+tar -xvf lmd_matched.tar.gz
+tar -xvf lmd_matched_h5.tar.gz
 
 rm lmd_matched.tar.gz
 rm lmd_matched_h5.tar.gz
